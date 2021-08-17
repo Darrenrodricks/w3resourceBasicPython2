c409bb205afcb1a10690cf34a7f4b9dfed6a205b
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-17 00:39:13 -0700

    Delete w3schoolbasics2 directory

--- w3schoolbasics2/sequenceChecker.py
+++
@@ -1,9 +0,0 @@
-# Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other.
-def sequence_checker(data):
-    if len(data) == len(set(data)):
-        return True
-    else:
-        return False
-
-print(sequence_checker([1,5,7,9]))
-print(sequence_checker([2,4,5,5,7,9]))
